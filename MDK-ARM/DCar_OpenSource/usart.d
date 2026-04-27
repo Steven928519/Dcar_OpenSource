3b511c6ab03289ec3a60ddf41ab36109de7f97d1
158398 <2122184824@qq.com>
2026-04-27 21:30:32 +0800

    keil_ba0a2dd

--- MDK-ARM/DCar_OpenSource/usart.d
+++ MDK-ARM/DCar_OpenSource/usart.d
@@ -8,7 +8,7 @@ dcar_opensource\usart.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Include/core_cm4.h
-dcar_opensource\usart.o: D:\keil555\ARM\ARMCC\Bin\..\include\stdint.h
+dcar_opensource\usart.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Include/cmsis_version.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -16,7 +16,7 @@ dcar_opensource\usart.o: ../Drivers/CMSIS/Include/mpu_armv7.h
 dcar_opensource\usart.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
 dcar_opensource\usart.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
 dcar_opensource\usart.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-dcar_opensource\usart.o: D:\keil555\ARM\ARMCC\Bin\..\include\stddef.h
+dcar_opensource\usart.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 dcar_opensource\usart.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
 dcar_opensource\usart.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
 dcar_opensource\usart.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
