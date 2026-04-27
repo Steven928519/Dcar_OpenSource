a1799ec762479920e8bb35ed5cff805a1385be33
158398 <2122184824@qq.com>
2026-04-27 21:26:40 +0800

    keil_24dcfe3

--- MDK-ARM/DCar_OpenSource/motor_control.d
+++ MDK-ARM/DCar_OpenSource/motor_control.d
@@ -1,5 +1,6 @@
 dcar_opensource\motor_control.o: ../Core/Src/motor_control.c
 dcar_opensource\motor_control.o: ../Core/Inc/motor_control.h
+dcar_opensource\motor_control.o: ../Core/Inc/encoder.h
 dcar_opensource\motor_control.o: ../Core/Inc/main.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
 dcar_opensource\motor_control.o: ../Core/Inc/stm32f4xx_hal_conf.h
@@ -8,7 +9,7 @@ dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Include/core_cm4.h
-dcar_opensource\motor_control.o: D:\keil555\ARM\ARMCC\Bin\..\include\stdint.h
+dcar_opensource\motor_control.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Include/cmsis_version.h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -16,7 +17,7 @@ dcar_opensource\motor_control.o: ../Drivers/CMSIS/Include/mpu_armv7.h
 dcar_opensource\motor_control.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-dcar_opensource\motor_control.o: D:\keil555\ARM\ARMCC\Bin\..\include\stddef.h
+dcar_opensource\motor_control.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
@@ -29,10 +30,10 @@ dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
 dcar_opensource\motor_control.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
-dcar_opensource\motor_control.o: ../Core/Inc/encoder.h
+dcar_opensource\motor_control.o: ../Core/Inc/gpio.h
 dcar_opensource\motor_control.o: ../Core/Inc/pid.h
 dcar_opensource\motor_control.o: ../Core/Inc/tim.h
-dcar_opensource\motor_control.o: ../Core/Inc/gpio.h
